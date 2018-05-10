841336a2888b734339874b994f02e0316bb70bf5
David Dawson <daws23@gmail.com>
2018-05-09 22:28:21 -0700

    Updates

--- src/util.js
+++
@@ -1,8 +0,0 @@
-const imagePath = (file) => {
-  const prefix = (process.env.NODE_ENV === 'production') ? '/owens-list-quiz/images/' : '/images/';
-  return `${prefix}${file}`;
-}
-
-export {
-  imagePath
-};
