9251d3defa72499e5a44583957a3f65f068c2fdc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-10 04:26:16 +0000

    Deploy daws/owens-list-quiz to github.com/daws/owens-list-quiz.git:gh-pages

---
+++ src/Results.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { imagePath } from './util';
+import './Results.css';
+
+class Results extends React.Component {
+
+  render() {
+    const { questions, answers } = this.props;
+    const correct = answers.filter(a => a).length;
+
+    return (
+      <div className="results--wrapper">
+        <h1>You got {correct}/{answers.length} right!</h1>
+        {questions.map((question, i) => (
+          <div key={question} className="results--item">
+            <img src={`${imagePath(answers[i] ? 'check' : 'x')}.svg`} />
+            <p>{question}</p>
+          </div>
+        ))}
+        <h1 className="results--sign-up-title">Join Owen's List and make a difference</h1>
+        <a className="button button-large block-center" href="https://owenslist.us17.list-manage.com/subscribe?u=20068737de57bb81d3a7aa4b4&id=e4e5f0287e">Sign Up!</a>
+      </div>
+    );
+  }
+
+}
+
+export default Results;
