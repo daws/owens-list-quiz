7f91a2a49aabb63ed9c52ba9deb6dc6579fb28c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-10 04:14:43 +0000

    Deploy daws/owens-list-quiz to github.com/daws/owens-list-quiz.git:gh-pages

---
+++ src/Intro.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './Intro.css';
+import IntroMaterial from './IntroMaterial';
+
+class Intro extends React.Component {
+
+  handleStart = (e) => {
+    e.preventDefault();
+    this.props.onUpdateStage('quiz');
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <IntroMaterial image="styrofoam.jpg">Styrofoam</IntroMaterial>
+          <IntroMaterial image="clothes.jpg">Clothes</IntroMaterial>
+          <IntroMaterial image="paint.jpg">Paint</IntroMaterial>
+          <IntroMaterial image="batteries.jpg">Batteries</IntroMaterial>
+        </div>
+        <h1 className="heading">Can you recycle better than a Kindergartener?</h1>
+        <div className="center">
+          <a className="button button-large block-center" href="#" onClick={this.handleStart}>Ready? Go!</a>
+        </div>
+        <div className="intro--debug">{JSON.stringify(process.env)}</div>
+      </div>
+    );
+  }
+
+}
+
+export default Intro;
---
+++ src/Quiz.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './Quiz.css';
+
+class Quiz extends React.Component {
+
+  handleClick = (answer, e) => {
+    e.preventDefault();
+    const isCorrect = answer === this.props.content.correctAnswer;
+    this.props.onAnswer(isCorrect);
+  }
+
+  render() {
+    const { image, title, description, question, answers } = this.props.content;
+    return (
+      <div>
+        <div className="left-col">
+          <div className="quiz--image" style={{ backgroundImage: `url(/owens-list-quiz/images/${image})` }}></div>
+          <div className="quiz--title">{title}</div>
+        </div>
+        <div className="right-col">
+          <div className="quiz--description">{description}</div>
+          <div className="quiz--question">
+            <p>{question}</p>
+            {answers.map(answer => (
+              <a href="#" className="quiz--answer" key={answer} onClick={this.handleClick.bind(null, answer)}>{answer}</a>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Quiz;
